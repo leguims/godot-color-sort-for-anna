e3a46f3eaea4cce1ff6259d6c18a1b8933e26cd6
leguims <leguims@free.fr>
2025-02-01 15:01:22 +0100

    Ajout de la page des references.

--- Scenes/References/references.gd
+++ Scenes/References/references.gd
@@ -1,15 +1,15 @@
-extends Control
+extends Node
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	set_process_input(true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
-func _on_bouton_références_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/References/references.tscn")
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		get_tree().change_scene_to_file("res://Scenes/MenuPrincipal/menu_principal.tscn")
